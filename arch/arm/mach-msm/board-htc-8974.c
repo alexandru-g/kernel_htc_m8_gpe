bf763499f5cdff640182e71f68a2268b4dd1da43
Alexandru Gherghina <alexandru.gherghina.p@gmail.com>
2014-09-02 20:50:29 +0300

    Finished patching kernel with kexec-hardboot

--- arch/arm/mach-msm/board-htc-8974.c
+++ arch/arm/mach-msm/board-htc-8974.c
@@ -102,7 +102,6 @@ static int mhl_usb_sw_gpio;
 #else
 #define HTC_8974_PERSISTENT_RAM_SIZE (SZ_1M - SZ_128K)
 #endif
-#define HTC_8974_RAM_CONSOLE_SIZE    HTC_8974_PERSISTENT_RAM_SIZE
 
 static struct persistent_ram_descriptor htc_8974_persistent_ram_descs[] = {
 	{
@@ -117,6 +116,11 @@ static struct persistent_ram htc_8974_persistent_ram = {
 	.num_descs = ARRAY_SIZE(htc_8974_persistent_ram_descs),
 	.descs     = htc_8974_persistent_ram_descs,
 };
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+#define HTC_8974_HARDBOOT_PHYS (HTC_8974_PERSISTENT_RAM_PHYS - SZ_1M)
+#define HTC_8974_HARDBOOT_SIZE SZ_1M
+#endif
 
 #ifdef CONFIG_HTC_BUILD_EDIAG
 #define MSM_HTC_PMEM_EDIAG_BASE 0x05BD0000
@@ -699,7 +703,14 @@ static void __init htc_8974_map_io(void)
 
 void __init htc_8974_init_early(void)
 {
-	
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page
+	int ret = memblock_remove(HTC_8974_HARDBOOT_PHYS, HTC_8974_HARDBOOT_SIZE);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	persistent_ram_early_init(&htc_8974_persistent_ram);
 
 #ifdef CONFIG_HTC_DEBUG_FOOTPRINT
--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -49,6 +49,10 @@
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/kexec.h>
+#endif
+
 #define HTC_SMEM_PARAM_BASE_ADDR 0xFBF0000
 
 #define HW_RST_ADDR 0x270
@@ -490,6 +494,17 @@ void msm_restart(char mode, const char *cmd)
 	printk(KERN_ERR "Restarting has failed\n");
 }
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+static void msm_kexec_hardboot_hook(void)
+{
+	// Set PMIC to restart-on-poweroff
+	pm8xxx_reset_pwr_off(1);
+
+	turn_off_mdm_power();
+	turn_off_qsc_power();
+}
+#endif
+
 static int __init msm_pmic_restart_init(void)
 {
 	int rc;
@@ -506,6 +521,10 @@ static int __init msm_pmic_restart_init(void)
 	} else {
 		pr_warn("no pmic restart interrupt specified\n");
 	}
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+	kexec_hardboot_hook = msm_kexec_hardboot_hook;
+#endif
 
 	return 0;
 }
